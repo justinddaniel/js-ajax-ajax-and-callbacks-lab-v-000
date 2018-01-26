f81a4f26f84ea6612f466cf6e8b31acdec715c52
Justin Daniel <justinddaniel@gmail.com>
2018-01-26 21:39:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var displayError = () => $('#errors').html("Error: please try again")
 
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
 $(document).ready(function (){
 });
